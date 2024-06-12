254017f2edeac07f70da362d51dbf3952f4a4cd2
fborjaz <fborjaz@unemi.edu.ec>
2024-06-11 21:22:17 -0500

    style index & nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
-    "./templates/**/*.html", // Asegúrate de que esta ruta sea correcta
+    "./templates/**/*.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
 
